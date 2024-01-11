90ef760ab8b550cd4cf3cdd1ab070d073e95be2a
similicious <mail@similicious.de>
2024-01-11 10:46:53 +0100

    add ryclt alias

--- zsh/conf/rebuy.zsh
+++ zsh/conf/rebuy.zsh
@@ -5,6 +5,7 @@ if [[ "$(hostname)" == "rebuy-s.wienecke" ]]; then
     alias regrant="~/repos/vagrant/regrant"
     alias re="regrant"
     alias rep="git -C ~/repos/vagrant pull"
+    alias ryclt="~/repos/rebuy-clt/bin/console"
 
     # regrant completions
     fpath=(~/.rebuy/completion $fpath)
