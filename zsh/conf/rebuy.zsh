39ded32b4e84a936c5679a03aa5d0c1c5e6c147f
similicious <mail@similicious.de>
2024-04-11 23:07:16 +0200

    add dnsfix

--- zsh/conf/rebuy.zsh
+++ zsh/conf/rebuy.zsh
@@ -7,6 +7,9 @@ if [[ "$(hostname)" == "rebuy-s.wienecke" ]]; then
     alias rep="git -C ~/repos/vagrant pull"
     alias ryclt="~/repos/rebuy-clt/bin/console"
 
+    # dns
+    alias dnsfix='source $DOTFILES_DIR/secrets && sudo mv "$( [ -f /etc/resolver/$SECRETS_DOMAIN ] && echo /etc/resolver/$SECRETS_DOMAIN || echo /etc/resolver/${SECRETS_DOMAIN}.bak)" "$( [ -f /etc/resolver/$SECRETS_DOMAIN ] && echo /etc/resolver/${SECRETS_DOMAIN}.bak || echo /etc/resolver/$SECRETS_DOMAIN)" &&  sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder && ls /etc/resolver'
+
     # regrant completions
     fpath=(~/.rebuy/completion $fpath)
 fi
