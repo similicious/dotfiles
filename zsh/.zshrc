0650b49207ceb69a65c6dc154eb7afc8aaee4206
similicious <mail@similicious.de>
2026-09-03 09:43:56 +0200

    zsh: improve dnsfix

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -2,12 +2,12 @@
 [[ -e ${ZDOTDIR:-~}/.antidote ]] ||
   git clone https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
 
-# Loop through each .zsh file in the conf directory and source it
-for file in $ZDOTDIR/conf/*.zsh; do
-    source $file
-done
-
 autoload -Uz promptinit && promptinit
 autoload -Uz compinit; compinit
 source ${ZDOTDIR}/.antidote/antidote.zsh
 antidote load
+
+# Loop through each .zsh file in the conf directory and source it
+for file in $ZDOTDIR/conf/*.zsh; do
+    source $file
+done
--- zsh/conf/rebuy.zsh
+++ zsh/conf/rebuy.zsh
@@ -21,9 +21,6 @@ if [[ "$(hostname)" == "s-wienecke-rebuy" ]]; then
 
     alias wsdk="cd $WEB_PATH/web-sdk"
     
-    # dns
-    alias dnsfix='source $DOTFILES_DIR/secrets && sudo mv "$( [ -f /etc/resolver/$SECRETS_DOMAIN ] && echo /etc/resolver/$SECRETS_DOMAIN || echo /etc/resolver/${SECRETS_DOMAIN}.bak)" "$( [ -f /etc/resolver/$SECRETS_DOMAIN ] && echo /etc/resolver/${SECRETS_DOMAIN}.bak || echo /etc/resolver/$SECRETS_DOMAIN)" &&  sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder && ls /etc/resolver'
-
     # regrant completions
     fpath=(~/.rebuy/completion $fpath)
 
